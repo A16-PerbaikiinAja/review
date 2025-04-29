fa5246ffa029db32cbd3e1f22133ebd64227df6a
halizaarfa <halizansa@gmail.com>
2025-04-29 12:00:26 +0700

    [RED] Add tests for Review model

--- src/test/java/id/ac/ui/cs/advprog/review/model/ReviewTest.java
+++ src/test/java/id/ac/ui/cs/advprog/review/model/ReviewTest.java
@@ -1,99 +1,172 @@
 package id.ac.ui.cs.advprog.review.model;
 
-import enums.Status;
-import id.ac.ui.cs.advprog.review.state.ApprovedState;
-import id.ac.ui.cs.advprog.review.state.PendingState;
-import id.ac.ui.cs.advprog.review.state.RejectedState;
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReviewTest {
-
-    private Review review;
+    private UUID reviewId;
+    private UUID userId;
+    private UUID technicianId;
 
     @BeforeEach
-    public void setUp() {
-        review = new Review("1", 5, "Pengerjaannya oke nih!", "4321", "box127");
+    void setUp() {
+        reviewId = UUID.randomUUID();
+        userId = UUID.randomUUID();
+        technicianId = UUID.randomUUID();
     }
 
     @Test
-    public void testInitialState() {
-        assertEquals(Status.PENDING, review.getReviewStatus());
-        assertEquals("PENDING", review.getStateString());
-        assertEquals(PendingState.class, review.getState().getClass());
+    void testCreateValidReview() {
+        String comment = "Pengerjaannya sangat oke!";
+        int rating = 5;
+
+        Review review = Review.builder()
+                .id(reviewId)
+                .userId(userId)
+                .technicianId(technicianId)
+                .comment(comment)
+                .rating(rating)
+                .build();
+
+        assertEquals(reviewId, review.getId());
+        assertEquals(userId, review.getUserId());
+        assertEquals(technicianId, review.getTechnicianId());
+        assertEquals(comment, review.getComment());
+        assertEquals(rating, review.getRating());
+        assertNotNull(review.getCreatedAt());
     }
 
     @Test
-    public void testApprove() {
-        review.approve();
-        assertEquals(Status.APPROVED, review.getReviewStatus());
-        assertEquals("APPROVED", review.getStateString());
-        assertEquals(ApprovedState.class, review.getState().getClass());
+    void testIdIsMissing() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            Review.builder()
+                    // .id(null)
+                    .userId(userId)
+                    .technicianId(technicianId)
+                    .comment("Pengerjaannya sangat oke!")
+                    .rating(5)
+                    .build();
+        });
+
+        assertEquals("Review id is required", exception.getMessage());
     }
 
     @Test
-    public void testReject() {
-        review.reject();
-        assertEquals(Status.REJECTED, review.getReviewStatus());
-        assertEquals("REJECTED", review.getStateString());
-        assertEquals(RejectedState.class, review.getState().getClass());
+    void testUserIdIsMissing() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            Review.builder()
+                    .id(reviewId)
+                    // .userId(null)
+                    .technicianId(technicianId)
+                    .comment("Pengerjaannya sangat oke!")
+                    .rating(5)
+                    .build();
+        });
+
+        assertEquals("User id is required", exception.getMessage());
     }
 
     @Test
-    public void testGetId() {
-        assertEquals("1", review.getReviewId());
+    void testTechnicianIdIsMissing() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            Review.builder()
+                    .id(reviewId)
+                    .userId(userId)
+                    // .technicianId(null)
+                    .comment("Pengerjaannya sangat oke!")
+                    .rating(5)
+                    .build();
+        });
+
+        assertEquals("Technician id is required", exception.getMessage());
     }
 
     @Test
-    public void testGetRating() {
-        assertEquals(5, review.getRatingScore());
+    void testCommentIsMissing() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            Review.builder()
+                    .id(reviewId)
+                    .userId(userId)
+                    .technicianId(technicianId)
+                    // .comment(null)
+                    .rating(5)
+                    .build();
+        });
+
+        assertEquals("Comment is required", exception.getMessage());
     }
 
     @Test
-    public void testGetComment() {
-        assertEquals("Pengerjaannya oke nih!", review.getReview());
+    void testCommentIsEmpty() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            Review.builder()
+                    .id(reviewId)
+                    .userId(userId)
+                    .technicianId(technicianId)
+                    .comment("") // Empty
+                    .rating(5)
+                    .build();
+        });
+
+        assertEquals("Comment is required", exception.getMessage());
     }
 
     @Test
-    public void testGetUserId() {
-        assertEquals("4321", review.getUserId());
+    void testRatingLowerBoundary() {
+        Review review = Review.builder()
+                .id(reviewId)
+                .userId(userId)
+                .technicianId(technicianId)
+                .comment("Pengerjaannya sangat oke!")
+                .rating(1)
+                .build();
+
+        assertEquals(1, review.getRating());
     }
 
     @Test
-    public void testGetSubscriptionBoxId() {
-        assertEquals("box127", review.getSubscriptionBoxId());
+    void testRatingUpperBoundary() {
+        Review review = Review.builder()
+                .id(reviewId)
+                .userId(userId)
+                .technicianId(technicianId)
+                .comment("Pengerjaannya sangat oke!")
+                .rating(5)
+                .build();
+
+        assertEquals(5, review.getRating());
     }
 
     @Test
-    public void testSetId() {
-        review.setReviewId("2");
-        assertEquals("2", review.getReviewId());
+    void testRatingIsInvalid() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            Review.builder()
+                    .id(reviewId)
+                    .userId(userId)
+                    .technicianId(technicianId)
+                    .comment("Pengerjaannya sangat oke!")
+                    .rating(-1) // Invalid
+                    .build();
+        });
+
+        assertEquals("Rating must be between 1 and 5", exception.getMessage());
     }
 
     @Test
-    public void testSetRating() {
-        review.setRatingScore(4);
-        assertEquals(4, review.getRatingScore());
-    }
+    void testCreatedAt() {
+        Review review = Review.builder()
+                .id(reviewId)
+                .userId(userId)
+                .technicianId(technicianId)
+                .comment("Pengerjaannya sangat oke!")
+                .rating(5)
+                .build();
 
-    @Test
-    public void testSetComment() {
-        review.setReview("Pengerjaannya oke nih!");
-        assertEquals("Pengerjaannya oke nih!", review.getReview());
+        assertNotNull(review.getCreatedAt());
     }
-
-    @Test
-    public void testSetUserId() {
-        review.setUserId("4321");
-        assertEquals("4321", review.getUserId());
-    }
-
-    @Test
-    public void testSetSubscriptionBoxId() {
-        review.setSubscriptionBoxId("box127");
-        assertEquals("box127", review.getSubscriptionBoxId());
-    }
-
 }
