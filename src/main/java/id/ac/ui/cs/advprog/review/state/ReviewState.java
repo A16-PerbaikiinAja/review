ebd283450510146681b735bca2810453fee97c3d
halizaarfa <halizansa@gmail.com>
2025-04-29 11:22:50 +0700

    [REFACTOR] Change to Builder pattern

--- src/main/java/id/ac/ui/cs/advprog/review/state/ReviewState.java
+++
@@ -1,9 +0,0 @@
-package id.ac.ui.cs.advprog.review.state;
-
-import id.ac.ui.cs.advprog.review.model.Review;
-
-public interface ReviewState {
-    void approve(Review review);
-    void reject(Review review);
-    String getState();
-}
