83efc971f7b2572eae3a6deba2f3555eefcd1a6c
halizaarfa <halizansa@gmail.com>
2025-05-22 18:50:19 +0700

    [GREEN] Create TechnicianRepository and update ReviewRepository to support Technician

--- src/main/java/id/ac/ui/cs/advprog/review/repository/ReviewRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/review/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package id.ac.ui.cs.advprog.review.repository;
 
 import id.ac.ui.cs.advprog.review.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,10 @@ import java.util.UUID;
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
     List<Review> findByTechnicianId(UUID technicianId);
     List<Review> findByUserId(UUID userId);
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.technicianId = :technicianId")
+    Double findAverageRatingByTechnicianId(@Param("technicianId") UUID technicianId);
+
+    @Query("SELECT COUNT(r) FROM Review r WHERE r.technicianId = :technicianId")
+    Integer countByTechnicianId(@Param("technicianId") UUID technicianId);
 }
