ea3a4d91c5c4683904d4dfdea316f581103d9e19
halizaarfa <halizansa@gmail.com>
2025-05-25 13:25:37 +0700

    [REFACTOR] Fix code quality, delete unused import

--- src/main/java/id/ac/ui/cs/advprog/review/controller/TechnicianRatingController.java
+++ src/main/java/id/ac/ui/cs/advprog/review/controller/TechnicianRatingController.java
@@ -4,7 +4,11 @@ import id.ac.ui.cs.advprog.review.dto.TechnicianRatingDTO;
 import id.ac.ui.cs.advprog.review.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.UUID;
